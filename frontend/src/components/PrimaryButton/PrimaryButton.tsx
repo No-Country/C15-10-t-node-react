edcebbffa0094e7bbc7bf2a27e7aa34ad2c72d4e
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-12-01 18:12:01 -0500

    feat: Crear la sección de publicidad e instalar la biblioteca Lucide-React para los iconos

---
+++ frontend/src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,33 @@
+import Icon from "../Icon/Icon";
+
+interface PrimaryButtonProps {
+  text: string;
+  bgColor?: "black" | "white";
+  url: string;
+  iconName?: string;
+}
+
+function PrimaryButton({
+  bgColor = "black",
+  text,
+  url,
+  iconName,
+}: Readonly<PrimaryButtonProps>) {
+  const colorVariants = {
+    black: "bg-black text-white hover:bg-gray-900 hover:text-white",
+    white: "bg-white text-dark hover:bg-gray-100 hover:text-dark",
+  };
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <button
+        className={`flex items-center ${colorVariants[bgColor]} font-bold py-3 px-5 transition duration-300 ease-in-out rounded-full`}
+      >
+        {iconName ? <Icon name={iconName} size={20} /> : <></>}
+        <span className="ml-2">{text}</span>
+      </button>
+    </a>
+  );
+}
+
+export default PrimaryButton;
