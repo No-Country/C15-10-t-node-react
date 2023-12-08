7f6e33e1c2c96d12f33ee8641b0d7cf6133f7b79
luismagnum <luismagnum1@gmail.com>
2023-12-07 23:31:25 -0300

    first commit

--- frontend/src/layouts/Home/Home.tsx
+++ frontend/src/layouts/Home/Home.tsx
@@ -1,10 +1,14 @@
 import Footer from "../../components/Footer/Footer";
+import Hero from "../../components/Hero/Hero";
+import Navbar from "../../components/Navbar/Navbar";
 import Publicity from "../../components/Publicity/Publicity";
 import FeaturedDestinations from "./components/FeaturedDestinations/FeaturedDestinations";
 
-function Home() {
+function Home() { 
   return (
-    <div className="container-md mx-auto lg:max-w-screen-lg">
+    <div>
+      <Navbar />
+      <Hero />
       <FeaturedDestinations />
       <Publicity />
       <Footer />
