92f57c02abd7b0715b1daacba24a3eac169bf957
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-12-10 18:20:32 -0500

    feat: Implementación de la sección de Preguntas Frecuentes (FAQ)

--- frontend/src/layouts/Home/Home.tsx
+++ frontend/src/layouts/Home/Home.tsx
@@ -2,6 +2,7 @@ import Footer from "../../components/Footer/Footer";
 import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
 import Publicity from "../../components/Publicity/Publicity";
+import FAQSection from "./components/FAQSection/FAQSection";
 import FeaturedDestinations from "./components/FeaturedDestinations/FeaturedDestinations";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <div className="container-md mx-auto lg:max-w-screen-lg overflow-hidden">
         <FeaturedDestinations />
         <Publicity />
+        <FAQSection />
       </div>
       <Footer />
     </>
