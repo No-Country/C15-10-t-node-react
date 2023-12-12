1ef72efc59c1f20c7bdd099133a6a06b6c4a06de
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2023-12-12 14:09:17 -0300

    redux tk & redux persist impl authSlice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/store.ts';
 import ScrolToTop from "./helpers/ScropToTop"
 
 
@@ -10,8 +13,12 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScrolToTop />
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ScrolToTop />
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
