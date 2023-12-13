9702f4a092bb800709a99064873af3353407c02e
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-12-13 15:37:43 -0500

    feat: Se agrego la funcionalidad de AutoComplete del Input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
 import Home from "./layouts/Home/Home";
 
 function App() {
+<<<<<<< Updated upstream
   return <Home />;
+=======
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/search" element={<Search></Search>}></Route>
+        <Route path="/place/:id" element={<Place></Place>}></Route>
+        <Route
+          path="/user/:id"
+          element={
+            user && user.token ? (
+              <Profile></Profile>
+            ) : (
+              <Navigate to="/login" replace={true} />
+            )
+          }
+        ></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ frontend/src/components/Nav/components/AutoComplete/AutoComplete.tsx
@@ -0,0 +1,113 @@
+import classNames from "classnames";
+import { MapPin, Search, MapPinned } from "lucide-react";
+import { memo, useEffect, useRef, useState } from "react";
+
+type Props = {
+  items: {
+    id: number;
+    name: string;
+    location: string;
+    image: string;
+    rating: number;
+    price: number;
+  }[];
+  value: string;
+  placeholder: string;
+  onChange(val: string): void;
+  onClick: (val: boolean, text: string) => void;
+};
+
+function AutoComplete(props: Readonly<Props>) {
+  const { items, value, onChange, placeholder, onClick } = props;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [query, setQuery] = useState(value);
+  const [open, setOpen] = useState(false);
+
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  const handleFocus = () => {
+    setOpen(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setOpen(false);
+      onClick(false, query);
+    }, 300);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, [open]);
+
+  return (
+    <div
+      className={classNames({
+        "dropdown w-full": true,
+        "dropdown-open": open,
+      })}
+    >
+      <div className="absolute inset-y-0 start-0 ps-3 cursor-pointer flex items-center">
+        <Search />
+      </div>
+      <input
+        ref={inputRef}
+        type="text"
+        className="block w-full p-5 ps-10 text-sm rounded-t-lg outline-none text-base-content border-base-content border-b-2  placeholder:text-gray-500 placeholder:text-sm focus:ring-inset sm:text-sm sm:leading-6"
+        value={value}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={placeholder}
+        tabIndex={0}
+        maxLength={32}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      <div className="dropdown-content bg-base-200 top-15 max-h-96 overflow-auto flex-col rounded-md z-40 w-full">
+        {filteredItems.length > 0 && (
+          <p className="text-start p-2 font-bold">Destinos Populares</p>
+        )}
+        <ul className="menu rounded-box w-full" tabIndex={0}>
+          {filteredItems.map((item, index) => (
+            <li
+              key={item.id}
+              tabIndex={index + 1}
+              onClick={() => {
+                onChange(item.name);
+                setQuery("");
+                setOpen(false);
+              }}
+              className="border-b border-b-base-content/10 w-full"
+            >
+              <a href={`/Search?q=${item.name}`}>
+                <div className="flex items-center">
+                  <MapPin className="mr-1" />
+                  <div>
+                    <p className="text-sm font-bold">{item.name}</p>
+                    <p className="text-xs">{item.location}</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+          ))}
+          {filteredItems.length === 0 && (
+            <li className="w-full">
+              <a href={`/search?q=${query}`}>
+                <Search /> Ver todos los resultados de {query}
+              </a>
+              <button>
+                <MapPinned /> Agregar un lugar faltante
+              </button>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default memo(AutoComplete);
